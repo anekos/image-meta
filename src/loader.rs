be4049f7f921374e3164528d9f81c01888cadf33
anekos <anekos@snca.net>
2019-03-11 22:59:49 +0900

    Add `loader::jpeg`

--- src/loader.rs
+++ src/loader.rs
@@ -1,7 +1,8 @@
 
-use std::io::{BufRead, Seek};
+use std::io::{BufRead, Seek, SeekFrom};
 
 pub mod gif;
+pub mod jpeg;
 pub mod png;
 
 use crate::errors::ImageError::InvalidSignature;
@@ -10,11 +11,20 @@ use crate::types::ImageMeta;
 
 
 
-pub fn load<R: BufRead + Seek>(image: &mut R) -> ImageResult<ImageMeta> {
-    match gif::load(image) {
-        Ok(meta) => return Ok(meta),
-        Err(InvalidSignature) => (),
-        otherwise => return otherwise,
+macro_rules! try_to_load {
+    ($image_type:ident, $image:ident) => {
+        match $image_type::load($image) {
+            Ok(meta) => return Ok(meta),
+            Err(InvalidSignature) => {
+                $image.seek(SeekFrom::Start(0))?;
+            },
+            otherwise => return otherwise,
+        }
     }
+}
+
+pub fn load<R: BufRead + Seek>(image: &mut R) -> ImageResult<ImageMeta> {
+    try_to_load!(gif, image);
+    try_to_load!(jpeg, image);
     png::load(image)
 }
---
+++ src/loader/jpeg.rs
@@ -0,0 +1,87 @@
+
+use std::io::{BufRead, Cursor, Seek, SeekFrom};
+
+use byteorder::{ReadBytesExt, BigEndian};
+
+use crate::errors::{ImageError, ImageResult, ImageResultU};
+use crate::types::{Dimensions, ImageMeta};
+
+
+
+const MARKER: u8 = 0xff;
+const SOI: u8 = 0xd8;
+
+
+pub fn load<R: BufRead + Seek>(image: &mut R) -> ImageResult<ImageMeta> {
+    read_signature(image)?;
+    let dimensions = read_sof(image)?;
+    Ok(ImageMeta { dimensions })
+}
+
+fn read_signature<R: BufRead + Seek>(image: &mut R) -> ImageResultU {
+    let mut soi = [0u8;2];
+    image.read_exact(&mut soi)?;
+    if [MARKER, SOI] != soi {
+        return Err(ImageError::InvalidSignature);
+    }
+    Ok(())
+}
+
+fn read_sof<R: BufRead + Seek>(image: &mut R) -> ImageResult<Dimensions> {
+    loop {
+        if let (_, Some(data)) = read_segment(image, is_sof)? {
+            let mut data = Cursor::new(data);
+            data.seek(SeekFrom::Current(1))?;
+            let height = data.read_u16::<BigEndian>().map(u32::from)?;
+            let width = data.read_u16::<BigEndian>().map(u32::from)?;
+            return Ok(Dimensions { width, height })
+        }
+    }
+}
+
+fn read_segment<R: BufRead + Seek, F>(image: &mut R, target_marker: F) -> ImageResult<(u8, Option<Vec<u8>>)>
+where F: Fn(u8) -> bool {
+    let prefix = image.read_u8()?;
+    if prefix != MARKER {
+        return Err(ImageError::CorruptImage("Marker not found"));
+    }
+
+    // Skip stuffing bytes
+    let mut marker = image.read_u8()?;
+    while marker == MARKER {
+        marker = image.read_u8()?;
+    }
+
+    let length = image.read_u16::<BigEndian>()? - 2;
+
+    if target_marker(marker) {
+        let mut result = vec![0u8; length as usize];
+        image.read_exact(&mut result)?;
+        Ok((marker, Some(result)))
+    } else {
+        image.seek(SeekFrom::Current(i64::from(length)))?;
+        Ok((marker, None))
+    }
+}
+
+fn is_sof(marker: u8) -> bool {
+    match marker {
+        0xc0 | 0xc1 | 0xc2 | 0xc3 | 0xc5 | 0xc6 | 0xc7 | 0xc9 |
+        0xca | 0xcb | 0xcd | 0xce | 0xcf => true,
+        _ => false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::loader::jpeg::load;
+
+    #[test]
+    fn test_load() {
+        let file = std::fs::File::open("test-files/paw.jpg").unwrap();
+        let mut file = std::io::BufReader::new(file);
+        let meta = load(&mut file).unwrap();
+        assert_eq!(meta.dimensions.width, 507);
+        assert_eq!(meta.dimensions.height, 370);
+    }
+}
