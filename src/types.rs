575ff757097494ef89e3a764c166ee1d4b283504
anekos <anekos@snca.net>
2019-03-13 21:33:31 +0900

    Derive some traits for `ImageMeta`

--- src/types.rs
+++ src/types.rs
@@ -2,17 +2,20 @@
 
 
 
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct ImageMeta {
     pub animation_frames: Option<usize>,
     pub dimensions: Dimensions,
     pub format: Format,
 }
 
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Dimensions {
     pub height: u32,
     pub width: u32,
 }
 
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Format {
     Gif,
     Jpeg,
